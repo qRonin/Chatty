10bf22cf78188f117eccbf1319b915ed74335d9e
qronin <michal.maron@edu.uekat.pl>
2018-06-04 17:08:21 +0200

    add buttonstyle; add hoverstyle; update windowviewmodel, temporarystyle in mainwindow.xaml;

--- Chatty.W/Chatty.W/ViewModel/WindowViewModel.cs
+++ Chatty.W/Chatty.W/ViewModel/WindowViewModel.cs
@@ -1,15 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Chatty.W
 {
     class WindowViewModel : BaseViewModel
     {
-        #region priv fields
+        #region Fields Private
         private Window mWindow;
         private int mOuterMarginSize = 10;
         private int mWindowRadius = 10;
         #endregion
-
+        #region Fields Public
         public int ResizeBorder {get; set; } = 6;
 
         public Thickness ResizeBorderThickness {get {return new Thickness(ResizeBorder); } }
@@ -45,8 +48,15 @@ namespace Chatty.W
         public int TitleHeight {get; set; }
 
         public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+        #endregion
+        #region Commans
+        public ICommand MinimizeCommand {get; set; }
+        public ICommand MaximizeCommand {get; set; }
+        public ICommand CloseCommand { get; set; }
+        public ICommand MenuCommand { get; set; }
+        #endregion
 
-
+        #region Ctor
         public WindowViewModel(Window window)
         {
             mWindow = window;
@@ -59,6 +69,22 @@ namespace Chatty.W
                 OnPropertyChanged(nameof(WindowRadius));
                 OnPropertyChanged(nameof(WindowCornerRadius));
             };
+
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Maximized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
         }
+        #endregion
+
+        #region Helpers
+
+        private Point GetMousePosition()
+        {
+            var position = Mouse.GetPosition(mWindow);
+            return new Point(position.X + mWindow.Left, position.Y + mWindow.Top);
+        }
+        #endregion
     }
 }
